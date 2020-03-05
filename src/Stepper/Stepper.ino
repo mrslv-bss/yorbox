ab3b5e99f20a56ee832445b58f9f58a004afb80f
Miroslav <61597763+BassTechnologies@users.noreply.github.com>
2020-03-05 21:30:43 +0200

    Update Stepper.ino

--- src/Stepper/Stepper.ino
+++ src/Stepper/Stepper.ino
@@ -4,33 +4,29 @@ CustomStepper stepper(8, 9, 10, 11);  // Подключаем
 int steppermode;  // Режим шагового двигателя
 long steppertimer;  // Замена delay
 
-void setup()
-{
+void setup()  {
   Serial.begin(9600);
   stepper.setRPM(12);
   stepper.setSPR(4075.7728395);
 }
 
-void loop()
-{
-
-first(180, true);  // Идём по часовой
-if (millis() - steppertimer >= 5000) {  // Ждём 5 секунд
-  steppertimer = millis();
-  first(180, false);  // Идём против часовой
+void loop() {
+indexstepper(180, true);  // Идём по часовой
+  if (millis() - steppertimer >= 5000) {  // Ждём 5 секунд
+    steppertimer = millis();
+    indexstepper(180, false);  // Идём против часовой
+  }
 }
 
-}
-
-void first(int tangaz, int wwd)  {
-  if (stepper.isDone() && steppermode == false && wwd == true)
+void indexstepper(int tangaz, int mode)  {
+  if (stepper.isDone() && steppermode == false && mode == true)
   {
     stepper.setDirection(CCW);
     stepper.rotateDegrees(tangaz);
     steppermode = true;
     Serial.println("По");
   }
-  else if (stepper.isDone() && steppermode == true && wwd == false)
+  else if (stepper.isDone() && steppermode == true && mode == false)
   {
     stepper.setDirection(CW);
     stepper.rotateDegrees(tangaz);
